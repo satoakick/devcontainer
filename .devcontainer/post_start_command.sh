d9164d2fa1eb5a190c9df569b53dc4df78d19bb7
satoakick <satoakick@gmail.com>
2022-05-09 02:14:23 +0000

    Move lsd installation into Dockerfile

--- .devcontainer/post_start_command.sh
+++ .devcontainer/post_start_command.sh
@@ -4,9 +4,3 @@ mkdir -p ${HOME}/.cache
 if [ ! -d ${HOME}/powerlevel10k ]; then
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${HOME}/powerlevel10k
 fi
-
-if ! type lsd > /dev/null 2>&1; then
-  wget https://github.com/Peltoche/lsd/releases/download/0.21.0/lsd_0.21.0_amd64.deb
-  sudo dpkg -i lsd_0.21.0_amd64.deb
-  rm lsd_0.21.0_amd64.deb
-fi
